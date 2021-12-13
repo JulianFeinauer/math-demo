1ed2a28c70c20d056118e9966c6a9e983343556d
julian <j.feinauer@pragmaticminds.de>
2021-12-13 16:11:27 +0100

    Initial Commit

---
+++ src/components/Question.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  This is a question:
+  <b>{{ x }} + {{ y }}</b>
+  =
+  <ion-input placeholder="?" v-model="input" @keyup="checkResult"></ion-input>
+  <div>
+    {{ message }}
+  </div>
+</div>
+</template>
+
+<script>
+import {IonInput} from "@ionic/vue";
+
+export default {
+  name: "Question",
+  components: { IonInput },
+  props: {
+    x: Number,
+    y: Number,
+    result: Number
+  },
+  data() {
+    return {
+      input: '',
+      message: "Asdf"
+    }
+  },
+  watch: {
+    "input": function(val) {
+      console.log("Result:", this.result, ", input", val)
+      if (this.result === val) {
+        this.message = "Yay!"
+      } else {
+        this.message = ":("
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Tab4.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 4 is here</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 3</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+      <Question x="5" y="4" result="9"/>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+// import ExploreContainer from '@/components/ExploreContainer.vue';
+import Question from '@/components/Question.vue';
+
+export default  {
+  name: 'Tab4',
+  components: { Question, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+}
+</script>
